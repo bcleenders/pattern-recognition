8613f1effad792ec0b8d9f60fd51fdd564c8130d
bcleenders <bcleenders@gmail.com>
2015-10-06 00:05:37 +0200

    Ran some tests, fixed stuff, added pics

--- assignment2/code/FeatureExtractor.m
+++ assignment2/code/FeatureExtractor.m
@@ -1,9 +1,24 @@
-figure = DrawCharacter(10);
+% Returns a vector of featurevectors.
+% Each featurevector has:
+% startX
+% startY
+% endX
+% endY
+% direction
+% maxdistance (detect circles)
+
+function lines = FeatureExtractor(maxtime)
+
+figure = DrawCharacter(maxtime);
 
 % Count the number of lines
 % Lines are separated by one or more zeroes (third row)
 numLines = 0;
 lastZero = true;
+minX = 0;
+minY = 0;
+maxX = 0.0000001; % No divide by zero error
+maxY = 0.0000001; % No divide by zero error
 for i = 1:length(figure)
     if (lastZero && figure(3, i) == 1)
         numLines = numLines +1;
@@ -12,36 +27,30 @@ for i = 1:length(figure)
     if (figure(3, i) == 0)
        lastZero = true; 
     end
+    if(figure(3, i) == 1)
+       minX = min(figure(1, i), minX);
+       minY = min(figure(2, i), minY);
+       maxX = max(figure(1, i), maxX);
+       maxY = max(figure(2, i), maxY);
+    end
 end
 
-trans = transpose(figure);
-
-mins = min(trans(trans(:, end) ~= 0, :));
-minX = mins(1);
-minY = mins(2);
-
-moved = figure + repmat([minX; minY; 0], 1, length(figure));
-
-maxes = max(moved(moved(:, end) ~= 0, :));
-maxX = maxes(1);
-maxY = maxes(2);
+% moved = figure ;
 
 % The factor we are going to scale with in x and y direction
 xscale = 1/((maxX - minX) + 0.00001);
 yscale = 1/((maxY - minY) + 0.00001);
 
-scaled = moved.*repmat([xscale; yscale; 1], 1, length(moved));
+scaled = (figure- repmat([minX; minY; 0], 1, length(figure))).*repmat([xscale; yscale; 1], 1, length(figure));
 
 % Clear some stuff from memory
 clear figure;
-clear trans;
-clear moved;
+% clear moved;
 
 % At this point, scaled has columns with three elements
 % For each element with e(3)==0, it holds that 0 <= e(1), e(2) <= 1
 
 lastZero = true;
-lineStart = [0; 0];
 lines = zeros(6, numLines);
 j = 0;
 linestart = 0;
@@ -56,7 +65,8 @@ for i = 1:length(scaled)
         
         lastZero = false;
     end
-    if (scaled(3, i) == 0 && ~lastZero || i == length(scaled))
+    % If the user release the mouse (this one is zero and the next one is
+    if ((scaled(3, i) == 0 && ~lastZero) || (scaled(3, i) == 1 && i == length(scaled)))
        lastZero = true;
        lines(3, j) = scaled(1, i);
        lines(4, j) = scaled(2, i);
@@ -67,6 +77,10 @@ for i = 1:length(scaled)
        % atan2(1, 0) = 1/2 pi   (up)
        % atan2(0, -1)= pi       (left)
        % atan2(-1, 0)= -1/2 pi  (down)
+       lines(4,j)
+       lines(2,j)
+       lines(3,j)
+       lines(1,j)
        lines(5, j) = atan2(lines(4, j) - lines(2, j) + 0.00001, lines(3, j) - lines(1, j) + 0.00001);
        
        
@@ -86,15 +100,3 @@ for i = 1:length(scaled)
        lines(6, j) = maxd;
     end
 end
-
-% Lines has:
-% startX
-% startY
-% endX
-% endY
-% direction
-% maxdistance (detect circles)
-
-lines
-bar(transpose(lines))
-
---
+++ assignment2/code/run.m
@@ -0,0 +1,4 @@
+l = FeatureExtractor(10);
+
+bar(transpose(l))
+l
