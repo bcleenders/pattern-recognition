e8fd776ec2dd51d856dde761861c0c5e163c9c55
bcleenders <bcleenders@gmail.com>
2015-10-09 14:39:56 +0200

    Started week 3

---
+++ assignment1/week3.m
@@ -0,0 +1,13 @@
+mc=MarkovChain([1;0], [0.9 0.1 0;0 0.9 0.1]);
+g1=GaussD('Mean',0,'StDev',1);
+g2=GaussD('Mean',3,'StDev',2);
+h=HMM(mc, [g1; g2]);
+
+x = [-0.2 2.6 1.3];
+c = [1, 0.1625 0.8266 0.0581];
+
+% Height equal to number of states, length equal to number of observations
+% pX(i, t) is P(X(t) = x(t) | S = i)
+pX = h.OutputDistr.prob(x);
+
+bh = mc.backward(pX, c);
