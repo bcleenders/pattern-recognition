736cc18a0d37d2d7000fd9d2f7ee0584073e1ef3
bcleenders <bcleenders@gmail.com>
2015-10-16 11:02:10 +0200

    Wrote Bs

--- assignment2/code/getInput.m
+++ assignment2/code/getInput.m
@@ -1,6 +1,6 @@
 maxtime = 5;
 
-chars = ['A'];% 'B' 'C' 'O'];% 'Q' 'P' 'b' 'T' '+' 'U' 'V' 'W'];
+chars = ['B'];% 'C'];% 'Q' 'P' 'b' 'T' '+' 'U' 'V' 'W'];
 
 for c=1:1:length(chars)
     for i=1:1:15
