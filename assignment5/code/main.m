41b4a1060412fd1be4de17fb5ee38ef1263913f3
bcleenders <bcleenders@gmail.com>
2015-11-01 14:09:45 +0100

    Blah blah still not working correctly

--- assignment5/code/main.m
+++ assignment5/code/main.m
@@ -1,14 +1,22 @@
-char = '+';
+char = 'T';
 
 charFeatures = [];
 lengths = [];
 
 for i= 1:1:12
-    load(sprintf('training_data/features_%s_%.2d', char, i), 'features')
-    figure1 = features;
-    charFeatures = [charFeatures, features];
-    lengths = [lengths, length(features)];
+    load(sprintf('training_data/features_%s_%.2d', char, i), 'features');
+    charFeatures = [charFeatures features];
+    lengths = [lengths length(features)];
 end
 
-nStates = 5;
-MakeLeftRightHMM(nStates, GaussD, charFeatures, lengths);
+for i= 1:1:length(charFeatures)
+    for j= 1:1:4
+        charFeatures(j, i) = charFeatures(j, i) + (rand() - 0.5)/100;
+    end
+end
+
+nStates = 3;
+hmm = MakeLeftRightHMM(nStates, GaussMixD(4), charFeatures, lengths);
+
+load(sprintf('training_data/features_%s_%.2d', char, 13), 'features');
+hmm.logprob(features)
